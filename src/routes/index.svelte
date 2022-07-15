2c93c4c2a774509ac1dfde0b0d4d2d31101e26a2
Alex Requeno <alexrequeno@gmail.com>
2022-07-15 16:56:41 -0600

    --

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,2 @@
-<script>
-    import Button, {Label} from '@smui/button';
-</script>
-<h1>Bienvenido a GM247 Administrador</h1>
-<Button color="secondary"  variant="raised">
-    <Label>Raised</Label>
-</Button>
+
+<h1>Grocery List A2R2</h1>
